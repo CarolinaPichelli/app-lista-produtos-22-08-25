dd5b4d47e0ec7c49f348cd7fd921995250ae71b9
Carolina Pichelli <144241856+CarolinaPichelli@users.noreply.github.com>
2025-08-29 16:19:27 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,20 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
 import { useEffect, useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-const [lista, setLista] = useState([])
-
-useEffect( async () => {
-  var resultado = await fetch('https://fakestoreapi.com/products')
-  setLista(await resultado.json())
-}, [] )
+import { ScrollView, StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
+  const [lista, setLista] = useState([])
+
+  useEffect(() => {
+    carregarDados();
+  },[])
+    
+  async function carregarDados() {
+    var resultado = await fetch('https://fakestoreapi.com/products');
+    var lst = await resultado.json();
+    setLista(lst);
+  }
+
   return (
+    <ScrollView>
     <View style={styles.container}>
-      {lista.map((prod) => <Text key={prod.id}>{prod.title}</Text> )}
+      {lista.map((prod) => 
+      <View style={styles.item}> 
+        <Image style={styles.image} source={{uri:prod.image}}></Image>
+        <Text style={styles.title} key={prod.id}>{prod.title}</Text>
+      </View> 
+    
+    )}
       <StatusBar style="auto" />
     </View>
+    </ScrollView>
   );
 }
 
@@ -24,5 +37,17 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  item: {
+    flex: 1,
+    flexDirection: 'row',   
+  },
+  title: {
+    fontSize: 15,
+    fontWeight: 'bold',
+  },
+  image: {
+    widht: 50,
+    height: 50,
   },
 });
